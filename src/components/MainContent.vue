5ff3d92adc060b9fa6a94d171398fb022ef24de8
Andrea Gulli <andreagulli11@gmail.com>
2024-09-19 22:47:41 +0200

    add cards components

---
+++ src/components/MainContent.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {      
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="card-wrapper">
+        <div class="card"></div>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
